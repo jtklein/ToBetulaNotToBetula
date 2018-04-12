2cd860b8f04688da30b38125e86136afc613b0a6
Johannes Klein <johannes.t.klein@gmail.com>
2018-04-12 20:22:50 +0200

    Train and use a filter model that searche for images that are labels

---
+++ filter_model/use-label-filter-model.py
@@ -0,0 +1,23 @@
+import turicreate as tc
+import os
+
+print "About to use model"
+
+# Load the filter label model
+model = tc.load_model('Classifier.model')
+
+# For each directory in the traiing_data set
+for x in os.listdir('../download_images/training_data'):
+    print "Predicting for images in %s" % x
+    
+    # Load images from this directory
+    data = tc.image_analysis.load_images('../download_images/training_data/%s' % x, with_path=True)
+
+    # Predict label on input images
+    data['predictions'] = model.predict(data)
+
+    # Write to file
+    output = open('%s.txt' % x, 'w')
+    for i in data:
+        output.write("%s %s\n" % (i['predictions'], i['path']) )
+    output.close()
